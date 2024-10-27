8c5346ec4139a54e6fdb141b4ae153dbdb6fb1cf
damriver <nobuya4596@hotmail.com>
2024-10-27 19:37:23 +0900

    初期ファイル

---
+++ js/variable.js
@@ -0,0 +1,20 @@
+//変数の宣言
+let usreName;
+
+//値の入力
+usreName='侍太郎';
+
+//変数の宣言・値の代入
+let usreNunmber=55;
+
+//コンソールへの出力
+console.log(usreName);
+console.log(usreNunmber);
+
+//値の再代入
+usreName='侍花子';
+usreNunmber=88;
+
++//コンソールへの出力
++console.log(usreName);
++console.log(usreNunmber); 
---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+//BMI計算
+//体重(kg)
+let Weight;
+Weight=68;
+
+//身長（m）
+let Height;
+Height=1.7;
+
+//BMI
+let Bmi;
+
+//BMI計算式
+Bmi=(Weight/(Height*Height));
+
+//コンソールへの出力
+console.log(Bmi);
